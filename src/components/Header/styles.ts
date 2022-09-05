f09099d74c01130ba33d4b410b3e3a65ead9c57f
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-05 16:14:28 -0300

    Add header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background: ${props => props.theme["gray-900"]};
+  padding: 2.5rem 0 7.5rem;
+`
+
+export const HeaderContent = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const NewTransactionButton = styled.button`
+  background: ${props => props.theme["green-500"]};
+  padding: 0.75rem 1.25rem;
+  border-radius: 6px;
+  font-weight: 700;
+  font-size: 1rem;
+  line-height: 1.6;
+  color: ${props => props.theme.white};
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background: ${props => props.theme["green-300"]};
+    transition: background-color 0.2s;
+  }
+`
