d2dcac3e2bb92f673fbd630d211903e775ac7cb4
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-06 15:22:56 -0300

    Style modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo.svg'
 import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
@@ -8,20 +9,12 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-      <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <NewTransactionButton>Nova transação</NewTransactionButton>
-        </Dialog.Trigger>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-        <Dialog.Portal>
-          <Dialog.Overlay />
-
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-
-            <Dialog.Close />
-          </Dialog.Content>
-        </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root> 
       </HeaderContent>
     </HeaderContainer>
